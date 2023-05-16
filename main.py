d4eaeebc252ff6660a88ef21e5855d4256590195
EnzoTrevisan <enzo.trev@hotmail.com>
2023-05-16 20:07:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import openai
+
+#OPENAI API KEY
+openai.api_key = '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'
+
+# função que envia uma solicitação a API e recebe a resposta
+def enviar_solicitacao(texto):
+    resposta = openai.Completion.create(
+        engine='text-davinci-003', #mecanismo de geração de texto a ser usado.
+        prompt=texto, #representa a pergunta/frase que você deseja usar para solicitar uma resposta do modelo.
+        max_tokens=120,  #número máximo de caracteres
+        n=1,  #número de respostas geradas.
+        stop=None, #especifica um token de parada personalizado para interromper a geração do texto.
+        temperature=0.7  #controla a aleatoriedade das respostas geradas. 
+    )
+    return resposta.choices[0].text.strip()
+
+# variável para controlar se é a primeira pergunta
+primeira_pergunta = True
+
+# loop principal do chatbot
+while True:
+    if primeira_pergunta:
+        entrada = input("Usuário: ")
+        primeira_pergunta = False
+    else:
+        entrada = input("Te ajudo em algo mais? ")
+
+        if entrada.lower() == 'não' or entrada.lower() == 'nao':
+            print("Certo, se você tiver mais alguma dúvida, fique à vontade para perguntar.")
+            break
+
+    if entrada.lower() == 'sair':
+        break
+
+    resposta = enviar_solicitacao(entrada)
+    print("Plusoft Chatbot: " + resposta)
---
+++ readme.md
@@ -0,0 +1,43 @@
+<h1 align="center"> Soul Coderz </h1>
+
+<p align="center">
+O projeto ChatGPT tem como objetivo melhorar o atendimento ao cliente da empresa Plusoft<br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto habits tracker" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Python
+- ChatGPT
+
+## 💻 Projeto
+
+O problema que o nosso projeto se propõe a resolver é de fornecer um atendimento personalizado e eficiente aos clientes, além de reduzir o tempo de resposta ao cliente e melhorar a escalabilidade do suporte.
+
+
+## 🎨 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/9Yf24vMRIv0joNQIyP2wAR/Projeto-ChatGPT?node-id=0%3A1&t=VUueDNrVA6wgD2RH-1).
+É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+--- 
+
+<br>
+Feito com ♥ by Soul Coderz
